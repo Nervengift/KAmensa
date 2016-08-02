ae2bed62caa04e70b3cca20b2a37a96a5fe6a838
Felix <mail@nicidienase.de>
2016-08-03 01:03:30 +0200

    handle closed lines

--- mensaplan.py
+++ mensaplan.py
@@ -17,14 +17,20 @@ header = KAMensa.key_to_name('adenauerring') + " " + str(date)
 print('*'*len(header) +'\n' + header + '\n' + '*'*len(header))
 
 for line in plan.keys('adenauerring'):
-    meal = plan.meal('adenauerring',line,date)
-    if meal != None :
-            # Linie
-            print('\n' + str(KAMensa.key_to_name(line)) + ':')
-            for item in meal:
-                if 'nodata' not in item.keys():
-                    print('|-'+ item['meal'] + ' ' + item['dish'] + ' ' + str(item['price_1']) + u'€ ' + item['info'])
-                else:
-                    print("No Data")
-    else:
-            print('No Data')
+	meal = plan.meal('adenauerring',line,date)
+	if meal != None :
+			# Linie
+			print('\n' + str(KAMensa.key_to_name(line)) + ':')
+			for item in meal:
+				print(item.keys())
+				if 'closing_start' not in item.keys():
+					if 'nodata' not in item.keys():
+						print('|-'+ item['meal'] + ' ' + item['dish'] + ' ' + str(item['price_1']) + u'€ ' + item['info'])
+					else:
+						print("No Data")
+				else:
+					close_start = datetime.datetime.fromtimestamp(int(item['closing_start'])).strftime('%d. %m. %Y')
+					close_end   = datetime.datetime.fromtimestamp(int(item['closing_end'])).strftime('%d. %m. %Y')
+					print("Closed from {} to {}".format(close_start,close_end))
+	else:
+			print('No Data')
